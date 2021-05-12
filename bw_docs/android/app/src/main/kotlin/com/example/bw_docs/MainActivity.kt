ebee5377503293431a80c21027e32ac620f59b40
HSSNPdS <hssnpds@gmail.com>
2021-05-12 11:50:59 -0300

    Aula1

---
+++ bw_docs/android/app/src/main/kotlin/com/example/bw_docs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bw_docs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bw_docs/lib/core/app_widget.dart
@@ -0,0 +1,12 @@
+import 'package:dev_quiz/home/homePage.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "BlueWeb Docs",
+      home: HomePage(),
+    );
+  }
+}
